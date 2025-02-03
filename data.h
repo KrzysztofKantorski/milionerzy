9ceb786f099f5526dc2d33a446252b89bb22b9f6
KrzysztofKantorski <127105505+KrzysztofKantorski@users.noreply.github.com>
2025-02-03 15:14:20 +0100

    Add files via upload

---
+++ data.h
@@ -0,0 +1,18 @@
+#ifndef DATA_H
+#define DATA_H
+#include <string>
+using namespace std;
+
+
+								// Deklaracja tablic pytañ, odpowiedzi oraz klucza odpowiedzi
+
+
+//tablica przechowuj¹ca pytania
+extern  string questions[71];
+
+//tablica przechowuj¹ca mo¿liwe odpowiedzi na pytania
+extern  string options[71][4];
+
+//tablica z kluczem odpowiedzi
+extern  string key[71];
+#endif
