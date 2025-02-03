3bb5907a0e51fa831bf5b11a9a1da1994b82165e
KrzysztofKantorski <127105505+KrzysztofKantorski@users.noreply.github.com>
2025-02-03 15:16:57 +0100

    Add files via upload

---
+++ UI.h
@@ -0,0 +1,20 @@
+#ifndef UI_H
+#define UI_H
+using namespace std;
+
+
+									// Deklaracja funkcji odpowiadaj¹cych za narysowanie elementów w konsoli
+
+
+//funkcja odpowiadaj¹ca za wyœwietlenie napisu ,,Milionerzy"
+extern  void napis();
+
+//funkcja wyœwietlaj¹ca studio milionerów
+extern  void studio();
+
+//funkcja wyœwietlaj¹ca uœmiechniêt¹ twarz, gdy u¿ytkownik wygrywa
+extern  void gameWon();
+
+//funkcja wyœwietlaj¹ca smutn¹ twarz, gdy u¿ytkownik przegrywa
+extern  void gameLost();
+#endif
